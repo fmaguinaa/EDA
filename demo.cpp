4e2651fa615c9e820963ea492371202ef0fef220
ecuadros <ecuadros@spc.org.pe>
2023-06-17 18:39:55 -0500

    DemoBinaryTree, DemoHash added

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // cout
+#include "demo.h"
+using namespace std;
+
+void DemoArray()
+{
+    cout << "Hello from DemoArray()" <<endl;
+}
+
+void DemoBinaryTree()
+{
+    cout << "Hello from DemoBinaryTree()" <<endl;
+}
+
+void DemoHash()
+{
+    cout << "Hello from DemoHash()" <<endl;
+}
+
---
+++ demo.h
@@ -0,0 +1,8 @@
+#ifndef __DEMO_H__
+#define __DEMO_H__
+
+void DemoArray();
+void DemoBinaryTree();
+void DemoHash();
+
+#endif // __DEMO_H__
