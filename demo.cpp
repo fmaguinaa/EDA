65f831ac45550a421c277fe316bf810b7d8db65b
ecuadros <ecuadros@spc.org.pe>
2023-06-23 22:04:53 -0500

    Encapsulated Array using struct

--- demo.cpp
+++ demo.cpp
@@ -4,27 +4,24 @@
 using namespace std;
 
 void DemoArray()
-{
-    int *pVect1 = nullptr;
-    int vcount1 = 0, vmax1 = 0;
-
+{   
     cout << "Hello from DemoArray()" <<endl;
     cout << "Vector #1()" <<endl;
-    create_array(pVect1, vcount1, vmax1);
+    CArray v1;
+    create_array(&v1);
     for(auto i = 0 ; i < 15 ; i++)
-        insert(pVect1, vcount1, vmax1, i); 
+        insert(&v1, i); 
 
     cout << "Vector #2()" <<endl;
-    int *pVect2 = nullptr;
-    int vcount2 = 0, vmax2 = 0;
-    create_array(pVect2, vcount2, vmax2);
+    CArray v2;
+    create_array(&v2);
     for(auto i = 0 ; i < 12 ; i++)
-        insert(pVect2, vcount2, vmax2, i); 
+        insert(&v2, i); 
     
-    print(pVect1, vcount1, vmax1);
-    destroy_array(pVect1, vcount1, vmax1);
-    print(pVect2, vcount2, vmax2);
-    destroy_array(pVect2, vcount2, vmax2);
+    print(&v1);
+    destroy_array(&v1);
+    print(&v2);
+    destroy_array(&v2);
 }
 
 void DemoBinaryTree()
