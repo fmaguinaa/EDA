117738f693f613778b0208cae0d6e00cbf160107
ecuadros <ecuadros@spc.org.pe>
2023-06-23 21:49:02 -0500

    Multiple arrays

--- array.h
+++ array.h
@@ -1,9 +1,9 @@
 #ifndef __ARRAY_H__
 #define __ARRAY_H__
 
-void create_array();
-void insert(int val);
-void print();
-void destroy_array();
+void create_array (int *&pVect, int &vcount, int &vmax);
+void insert       (int *&pVect, int &vcount, int &vmax, int val);
+void print        (int *&pVect, int &vcount, int &vmax);
+void destroy_array(int *&pVect, int &vcount, int &vmax);
 
 #endif // __ARRAY_H__
